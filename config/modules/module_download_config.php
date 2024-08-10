5d949b6414cdb23f835d3ef344cff6b3c4a68a4e
3cxc <81449257+3cxc@users.noreply.github.com>
2024-08-10 22:44:34 +0800

    Module Configs
    Signed-off-by: 3cxc <81449257+3cxc@users.noreply.github.com>

---
+++ config/modules/module_download_config.php
@@ -0,0 +1,11 @@
+<?php
+    #下载配置文件
+    #Download configuration file
+
+    #文件下载设置
+    #请将要提供的下载文件上传到static/file目录(需要手动创建)
+   $download_array = array(
+      "example" => "example.exe",
+      "example1" => "example1.zip",
+   );
+?>
---
+++ config/modules/module_query_config.php
@@ -0,0 +1,9 @@
+<?php
+    #查询系统配置文件
+
+    #配置查询类型对应的版本号
+    $QueryType_array = array(
+      "example" => "1.0",
+      "example1" => "2.0",
+   );
+?>
