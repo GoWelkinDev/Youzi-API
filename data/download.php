6a1a3f1cbce6059d5294146559fdbcc8aee95256
3cxc <81449257+3cxc@users.noreply.github.com>
2024-08-11 11:00:17 +0800

    Hide source download link
    Signed-off-by: 3cxc <81449257+3cxc@users.noreply.github.com>

--- data/download.php
+++ data/download.php
@@ -1,6 +1,7 @@
 <?php
    header('Content-type: application/json; charset=utf-8"');
 
+   require_once './function/function_json.php';
    require_once './function/function_query.php';
    require_once './function/function_foreach.php';
    require_once '../config/config_global.php';
@@ -16,9 +17,30 @@
 
    function download($tmp){
 
-      global $App_Website;
       global $download_array;
 
-      return(header("Location: ".$App_Website."/static/file/".$download_array[$tmp]));
+      // 文件路径，可以是相对路径或绝对路径
+      $file_path = "../static/file/".$download_array[$tmp];
+
+      // 确保文件存在
+      if (file_exists($file_path)) {
+
+         $filename = basename($file_path);
+         $file_size = filesize($file_path);
+
+         header("Content-Type: application/octet-stream");
+         header("Content-Transfer-Encoding: Binary");
+         header("Content-Disposition: attachment; filename=\"$filename\"");
+         header("Content-Length: $file_size");
+
+         //清除可能存在的旧缓存
+         if (ob_get_level()) {
+            ob_end_clean();
+         }
+
+         readfile($file_path);
+         exit;
+      }
+      http_response_code(404);
    }
 ?>
