a1ff088ead38ceaebf48cb510b8502109b72c0c5
3cxc <81449257+3cxc@users.noreply.github.com>
2025-03-31 14:29:02 +0800

    Reapply "feat!: 重新规划了接口调用路径"
    This reverts commit dc7679c063cd75fc250fac890c2ee0cc4e3fce77.

---
+++ .history/index_20250331141421.php
@@ -0,0 +1,28 @@
+<?php
+  header('Content-type: application/json; charset=utf-8"');
+
+  require_once('./data/function/function_json.php');
+  require_once('./config/config_global.php');
+
+  $Status = "OK";
+  $RunTime_Mode = "productionMode";
+
+  if($Anti_switch){
+    $Status = "Down";
+  }
+  if($Debug_mode){
+    $RunTime_Mode = "DebuggingMode";
+  }
+
+  #格式化数组为json并输出
+  $json_arr = array(
+    "Status" => $Status,
+    "Runtime-Mode" => $RunTime_Mode,
+    "Application-Author" => "3cxc",
+    "Application-Description" => $API_Description,
+    "Application-Version" => "3.4.4",
+    "Application-Owner" => $API_Owner);
+
+  exit(formatJson($json_arr));
+
+?>
